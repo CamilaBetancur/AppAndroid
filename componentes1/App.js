886307064bfbcf89ce5411ddbe286909b72046d5
Camila <camilabema1@gmail.com>
2025-02-26 19:03:41 -0500

    cambis

--- componentes1/App.js
+++ componentes1/App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style= {styles.texto}>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
@@ -17,4 +16,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    fontSize: 20,
+    color: '#333'
+  }
 });
